afdd5945f7fed4bf5f986c458b64b23d42a5f17a
Sergey Miachkov <miachkov2004@gmail.com>
2023-03-20 08:52:10 +0100

    airports command v1
    First version of the /airports command, with three counties to pick from

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ airports = fr_api.get_airports()
 import logging
 
 from telegram import __version__ as TG_VER
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 try:
     from telegram import __version_info__
@@ -26,7 +27,7 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
 from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
 
 # Enable logging
 logging.basicConfig(
@@ -60,6 +61,34 @@ async def airport(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     airports = fr_api.get_airports()
     await update.message.reply_text(airports[0])
 
+async def airports(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    keyboard = [
+        [InlineKeyboardButton("Poland", callback_data="Poland")],
+        [InlineKeyboardButton("Germany", callback_data="Germany")],
+        [InlineKeyboardButton("France", callback_data="France")],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Please choose a country: ", reply_markup=reply_markup)
+    
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
+
+    airports = fr_api.get_airports()
+    reply = ""
+    for airport in airports:
+        if airport["country"] == query.data:
+            reply += airport["name"] + "\n"
+            
+    await query.edit_message_text(text=reply)
+
 
 def main() -> None:
     """Start the bot."""
@@ -70,6 +99,8 @@ def main() -> None:
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("airport", airport))
+    application.add_handler(CommandHandler("airports", airports))
+    application.add_handler(CallbackQueryHandler(button))
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
