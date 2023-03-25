38a38b938af58724b971f8916e25ba021463349a
Andrii Venher <vengerandr@gmail.com>
2023-03-25 02:18:31 +0100

    Add webhook server code

--- src/main.py
+++ src/main.py
@@ -4,6 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import asyncio
+
+import uvicorn
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.routing import Route
+
 from FlightRadar24.api import FlightRadar24API
 
 fr_api = FlightRadar24API()
@@ -203,9 +211,7 @@ async def airport_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         return -1
 
 
-def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
+def build_bot():
     application = Application.builder().token(os.getenv("BOT_TOKEN")).build()
 
     conv_handler = ConversationHandler(
@@ -225,9 +231,53 @@ def main() -> None:
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    return application
 
-    # Run the bot until the user presses Ctrl-C
+
+async def start_webhook(application):
+    url = os.getenv("BOT_WEBHOOK_URL")
+    port = int(os.getenv("BOT_WEBHOOK_PORT"))
+
+    await application.bot.set_webhook(url=f"{url}/telegram")
+
+    async def telegram(request: Request) -> Response:
+        await application.update_queue.put(
+            Update.de_json(data=await request.json(), bot=application.bot)
+        )
+        return Response()
+
+    starlette_app = Starlette(
+        routes=[
+            Route("/telegram", telegram, methods=["POST"])
+        ]
+    )
+
+    webserver = uvicorn.Server(
+        config=uvicorn.Config(
+            app=starlette_app,
+            port=port,
+            use_colors=False,
+            host="127.0.0.1",
+        )
+    )
+
+    async with application:
+        await application.start()
+        await webserver.serve()
+        await application.stop()
+
+
+def start_pooling(application):
     application.run_polling()
+
+
+def main() -> None:
+    application = build_bot()
+
+    if os.getenv("BOT_USE_WEBHOOK") == "true":
+        asyncio.run(start_webhook(application))
+    else:
+        start_pooling(application)
 
 
 if __name__ == "__main__":
