35c9be5a416e95bc8f1d41a49a9bec931eba519d
Sergey Miachkov <miachkov2004@gmail.com>
2023-04-05 11:37:06 +0200

    no more global values
    I got rid of global values that interfered with concurrent usage of the bot

--- src/commands.py
+++ src/commands.py
@@ -10,44 +10,41 @@ countries = ['Italy', 'Spain', 'Denmark', 'Poland', 'Germany',
              'France', 'Portugal', 'Sweden', 'Finland',
              'China', 'Austria', 'Japan', 'Norway',
              'Switzerland', 'Netherlands', 'Brazil', 'Argentina']
-countries_page = 1
 countries_per_page = 4
-max_number_of_pages = len(countries) // countries_per_page
+max_number_of_countries_pages = len(countries) // countries_per_page
 
-airports_page = 1
-airports_per_page = 4
 airports = fr_api.get_airports()
-local_airports = []
-
-right_button = InlineKeyboardButton(">", callback_data="RIGHT")
-left_button = InlineKeyboardButton("<", callback_data="LEFT")
+airports_per_page = 4
 
 AIRPORTS, AIRPORT_INFO = range(2)
 
 
 # For navigating between countries list
-def countries_list_formatter():
+def countries_list_formatter(page_number):
     keyboard = []
-
-    for country in countries[(countries_page - 1) * countries_per_page:countries_page * countries_per_page]:
+    for country in countries[(page_number - 1) * countries_per_page:page_number * countries_per_page]:
         keyboard.append([InlineKeyboardButton(country, callback_data=country)])
 
-    reply_markup = add_arrows(keyboard, countries_page, max_number_of_pages)
+    reply_markup = add_arrows(keyboard, page_number, max_number_of_countries_pages, page_number)
     return reply_markup
 
 
 # For navigating between airports list
-def airports_list_formatter():
+def airports_list_formatter(page_number, country):
     keyboard = []
+    local_airports = get_local_airports(country)
 
-    for airport in local_airports[(airports_page - 1) * airports_per_page:airports_page * airports_per_page]:
+    for airport in local_airports[(page_number - 1) * airports_per_page:page_number * airports_per_page]:
         keyboard.append([InlineKeyboardButton(airport["name"], callback_data=airport["name"])])
 
-    reply_markup = add_arrows(keyboard, airports_page, len(local_airports) // airports_per_page)
+    reply_markup = add_arrows(keyboard, page_number, len(local_airports) // airports_per_page, page_number)
     return reply_markup
 
 
-def add_arrows(keyboard, cur_page, max_page):
+def add_arrows(keyboard, cur_page, max_page, page_number):
+    right_button = InlineKeyboardButton(">", callback_data=page_number+1)
+    left_button = InlineKeyboardButton("<", callback_data=page_number-1)
+
     if 1 < cur_page < max_page:
         keyboard.append([left_button, right_button])
     elif cur_page > 1:
@@ -57,6 +54,15 @@ def add_arrows(keyboard, cur_page, max_page):
 
     reply_markup = InlineKeyboardMarkup(keyboard)
     return reply_markup
+
+def get_local_airports(country) -> list:
+    local_airports = []
+
+    for airport in airports:
+        if airport["country"] == country:
+            local_airports.append(airport)
+
+    return local_airports
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -88,79 +94,59 @@ async def random_airport(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     await update.message.reply_text(text)
 
 
-async def countries_list(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def countries_list(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Echo the user message."""
-    reply_markup = countries_list_formatter()
+    reply_markup = countries_list_formatter(1)
     await update.message.reply_text("Please choose a country: ", reply_markup=reply_markup)
 
     return AIRPORTS
 
 
-async def airports_list(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def airports_list(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Parses the CallbackQuery and updates the message text."""
     query = update.callback_query
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     await query.answer()
-    global countries_page
 
     # Show more countries or show airports
-    if query.data == "RIGHT":
-        countries_page += 1
-        reply_markup = countries_list_formatter()
-
-        await query.edit_message_text("Please choose a country: ", reply_markup=reply_markup)
-        return AIRPORTS
-    elif query.data == "LEFT":
-        countries_page -= 1
-        reply_markup = countries_list_formatter()
+    if query.data.isdigit():
+        reply_markup = countries_list_formatter(int(query.data))
 
         await query.edit_message_text("Please choose a country: ", reply_markup=reply_markup)
         return AIRPORTS
     else:
+        context.user_data["country"] = query.data
         reply = f"Airports in {query.data}:\n"
-        for airport in airports:
-            if airport["country"] == query.data:
-                local_airports.append(airport)
 
-        reply_markup = airports_list_formatter()
+        reply_markup = airports_list_formatter(1, query.data)
         await query.edit_message_text(text=reply, reply_markup=reply_markup)
         return AIRPORT_INFO
 
 
-async def airport_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def airport_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Parses the CallbackQuery and updates the message text."""
     query = update.callback_query
-    global countries_page
-    global airports_page
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     await query.answer()
 
     reply = ''
+    country = context.user_data["country"]
 
-    if query.data == "RIGHT":
-        airports_page += 1
-        reply_markup = airports_list_formatter()
-
-        await query.edit_message_text(text=query.message.text, reply_markup=reply_markup)
-        return AIRPORT_INFO
-    elif query.data == "LEFT":
-        airports_page -= 1
-        reply_markup = airports_list_formatter()
+    if query.data.isdigit():
+        reply_markup = airports_list_formatter(int(query.data), country)
 
         await query.edit_message_text(text=query.message.text, reply_markup=reply_markup)
         return AIRPORT_INFO
     else:
+        local_airports = get_local_airports(country)
+
         for airport in local_airports:
             if airport["name"] == query.data:
                 reply = text_from_airport(airport)
-
-        countries_page = 1
-        airports_page = 1
-        local_airports.clear()
 
         await query.edit_message_text(text=reply)
         return -1
