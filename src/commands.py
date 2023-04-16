2c524161ed3b835f2a5c598c71b021d78c4a7fd9
Yakub <yakubyakubov2003@mail.ru>
2023-04-16 17:39:18 +0200

    top_airlines_chart command

--- src/commands.py
+++ src/commands.py
@@ -228,11 +228,16 @@ async def top_destinations_chart(update: Update, context: ContextTypes.DEFAULT_T
 
 async def top_origins_chart(update: Update, context: ContextTypes.DEFAULT_TYPE):
     airports_ranking = flight_service.get_top_origins()[:10]
-
     fig = plot_service.make_airports_ranking_chart(airports_ranking, 'Outgoing flights')
 
     await update.message.reply_photo(plot_to_bytes(fig), flight_service.format_airports_ranking(airports_ranking))
 
+
+async def top_airlines_chart(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    flights = fr_api.get_flights()
+    airlines_ranking=flight_service.top_air(flights)
+    fig=plot_service.make_airline_ranking_chart(airlines_ranking,"Total flights")
+    await update.message.reply_photo(plot_to_bytes(fig), flight_service.format_airlines_ranking(airlines_ranking))
 
 async def get_aircraft_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
--- src/services/flight_service.py
+++ src/services/flight_service.py
@@ -31,6 +31,27 @@ def get_top_destinations():
 def get_top_origins():
     return get_top_flights_by_iata(lambda flight: flight.origin_airport_iata)
 
+def top_air(flights) -> list:
+        local_flights = []
+        airlines = {}
+        for flight in flights:
+            try:
+                if flight.on_ground:
+                    continue
+                airline = flight.airline_iata
+                if airline == 'N/A':
+                    continue
+                if airline in airlines:
+                    airlines[airline] += 1
+                else:
+                    airlines[airline] = 1
+            except:
+                ##bad data
+                pass
+
+        airlines = dict(sorted(airlines.items(), key=lambda x: x[1], reverse=True))
+        top_airlines = list(airlines.items())[:10]
+        return top_airlines
 
 def format_airports_ranking(airports_ranking):
     text = ""
@@ -42,6 +63,19 @@ def format_airports_ranking(airports_ranking):
         text += f' ({airport["position"]["country"]["name"]})'
         text += ": "
         text += f'{airport_tuple[1]}'
+        text += "\n"
+        i += 1
+    return text
+
+def format_airlines_ranking(top_airlines):
+    i = 1
+    text=""
+    for air in top_airlines:
+        text += f'{i}. '
+        air_line = fr_api.get_airlines()
+        text += air_line[i]["Name"]
+        text += ": "
+        text += f'{air[1]}'
         text += "\n"
         i += 1
     return text
