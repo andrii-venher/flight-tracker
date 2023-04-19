99f5515a0b893c3869f2f35d23b1f98da3f2f9d3
DalerZakirov <kirill283.81.41@gmail.com>
2023-04-19 15:46:46 +0200

    Add is_delayed command

--- src/bot.py
+++ src/bot.py
@@ -11,7 +11,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 from commands import random_airport, search_airport, airports_list, airport_info, unknown_command_echo, \
     random_flight, random_airline, top_destinations, search_flight, flights_list, flight_info, top_origins, \
     top_destinations_chart, top_origins_chart, get_aircraft_image, get_flight_map, top_airlines, top_airlines_chart, \
-    flight_by_id, airline_by_icao, airport_by_icao, airport_by_iata
+    flight_by_id, airline_by_icao, airport_by_icao, airport_by_iata, is_delayed
 
 from services.markup_service import AIRPORTS, AIRPORT_INFO, FLIGHTS, FLIGHT_INFO
 
@@ -36,7 +36,8 @@ def build_bot():
             CommandHandler("flight_by_id", flight_by_id),
             CommandHandler("airline_by_icao", airline_by_icao),
             CommandHandler("airport_by_icao", airport_by_icao),
-            CommandHandler("airport_by_iata", airport_by_iata)
+            CommandHandler("airport_by_iata", airport_by_iata),
+            CommandHandler("is_delayed", is_delayed),
         ],
         states={
             AIRPORTS: [CallbackQueryHandler(airports_list)],
--- src/commands.py
+++ src/commands.py
@@ -252,6 +252,7 @@ async def get_flight_map(update: Update, context: ContextTypes.DEFAULT_TYPE):
     except:
         await update.message.reply_text("Data not found")
 
+
 async def airline_by_icao(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         chunks = update.message.text.split(' ')
@@ -277,6 +278,7 @@ async def airport_by_icao(update: Update, context: ContextTypes.DEFAULT_TYPE):
     except:
         await update.message.reply_text("Data not found")
 
+
 async def airport_by_iata(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         chunks = update.message.text.split(' ')
@@ -286,5 +288,29 @@ async def airport_by_iata(update: Update, context: ContextTypes.DEFAULT_TYPE):
         airport_iata = chunks[1]
         text = flight_service.airp_by_iata(airport_iata)
         await update.message.reply_text(text)
+    except:
+        await update.message.reply_text("Data not found")
+
+
+async def is_delayed(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        chunks = update.message.text.split(' ')
+        if len(chunks) < 2:
+            await update.message.reply_text("Please provide flight ID.")
+            return
+        flight_id = chunks[1]
+        details = fr_api.get_flight_details(flight_id)
+        if details["time"]["scheduled"]["arrival"] is None:
+            await update.message.reply_text("Data not found")
+        elif details["time"]["estimated"]["arrival"] is not None:
+            diff = details["time"]["estimated"]["arrival"] - details["time"]["scheduled"]["arrival"]
+            if diff > 0:
+                await update.message.reply_text(f"The flight is delayed by {diff} seconds")
+            elif diff < 0:
+                await update.message.reply_text(f"The flight will arrive earlier by {-diff} seconds")
+            else:
+                await update.message.reply_text("The flight arrives on time")
+        else:
+            await update.message.reply_text("Data not found")
     except:
         await update.message.reply_text("Data not found")
